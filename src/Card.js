45bd690237561f9a157073c5207c782347a1a1ca
Taioh Kubota <kubotataioh@gmail.com>
2022-07-10 13:08:09 -0400

    starting files and component files made

---
+++ src/Card.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+const Card = ({name,image}) =>{
+    return <img
+            alt = {name}
+            src = {image} />
+}
+
+export default Card
---
+++ src/Deck.js
@@ -0,0 +1,5 @@
+import React, {useState, useEffect, useRef} from "react"
+import Card from "./Card"
+import axios from "axios"
+
+const card_API = "http://deckofcardsapi.com/api/deck"
